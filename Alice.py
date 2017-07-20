dfd8d70ffe066908c1f1abd76ae804c4e116beb2
sikajipo <30323306+sikajipo@users.noreply.github.com>
2017-07-20 13:31:58 -0400

    Add files via upload

---
+++ Alice.py
@@ -0,0 +1,12 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> name = 'Bob'
+>>> age = 5
+>>> if anme =='Alice':
+	print('Hi, Alice.')
+	elif age < 12:
+		
+SyntaxError: invalid syntax
+>>> print('You are not Alice, kiddo.')
+You are not Alice, kiddo.
+>>> 
