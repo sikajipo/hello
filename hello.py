2f94c8b4c02aeb6067facbc4ed32c6340c90a5b7
sikajipo <30323306+sikajipo@users.noreply.github.com>
2017-07-20 13:20:20 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,27 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # This program says hello and asks for my name.
+>>> print('Hello world!')
+Hello world!
+>>> print('What is your name?')	# ask for their anme
+What is your name?
+>>> nyName = input()
+print('It is good to meet you, ' + myName)
+>>> print('The length of your name is:')
+The length of your name is:
+>>> print(len(myname))
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(len(myname))
+NameError: name 'myname' is not defined
+>>> myname = = input
+SyntaxError: invalid syntax
+>>> myname = input()
+print(len(myname))
+>>> print('What is your age?')	#ask for thier age
+What is your age?
+>>> print('What is your age?')	#ask for their age
+What is your age?
+>>> myAge = input()
+print('You will be ' + str(int(myAge) + 1 + ' in a year.')
+>>> 
